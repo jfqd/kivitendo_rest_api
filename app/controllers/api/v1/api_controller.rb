ebe3ad2ff175204fe7486a42447ec74b8f7e5843
jfqd <jfqd@blun.org>
2017-10-20 21:27:26 +0200

    fix http-basic

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,18 +1,12 @@
 module Api
   module V1
     class ApiController < ApplicationController
-      
+
       include ActionController::HttpAuthentication::Basic::ControllerMethods
-      before_action :authenticate
-      
-      private
-      
-      def authenticate
-        authenticate_with_http_basic do |username, password|
-          true # username == ENV['HTTP_USERNAME'] && password == ENV['HTTP_PASSWORD']
-        end
-      end
-      
+
+      http_basic_authenticate_with name: Rails.application.secrets[:http_user],
+                               password: Rails.application.secrets[:http_pwd]
+
     end
   end
 end
