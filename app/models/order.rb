3a0b8cd57d05401d66755acaf0c9622dd0013913
jfqd <jfqd@blun.org>
2021-04-07 11:50:39 +0200

    whitespace cleanup

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,11 @@
 class Order < ApplicationRecord
   self.table_name  = 'oe'
   self.primary_key = 'id'
-  
+
   before_save :set_validity_date, if: :quotation?
-  
+
   has_many :orderitems, foreign_key: :trans_id
-  
+
   # map usable to legacy fields-names
   legacy_mapper [
     [:order_number,  :ordnumber],
@@ -18,10 +18,10 @@ class Order < ApplicationRecord
     [:order_date,    :transdate],
     [:shop_number,   :cusordnumber]
   ]
-  
+
   accepts_nested_attributes_for :orderitems, allow_destroy: true,
                                              reject_if: :all_blank
-  
+
   def orderitems=(params)
     params.each do |c|
       if new_record?
@@ -36,7 +36,7 @@ class Order < ApplicationRecord
       end
     end
   end
-  
+
   def created_at
     I18n.l(self.itime, :format => Api::V1::ApiController.default_api_time_format) unless new_record?
   rescue
@@ -45,7 +45,7 @@ class Order < ApplicationRecord
   def created_at=(v)
     # do nothing
   end
-  
+
   def updated_at
     I18n.l(self.mtime, :format => Api::V1::ApiController.default_api_time_format) unless new_record?
   rescue
@@ -54,15 +54,15 @@ class Order < ApplicationRecord
   def updated_at=(v)
     # do nothing
   end
-  
+
   private
-  
+
   def set_validity_date
     self.reqdate_as_date = ( Time.now + 14.days ) if new_record?
   end
-  
+
   def quotation?
     self.ordnumber.blank?
   end
-  
+
 end
